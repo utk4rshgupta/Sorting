701fe93890f54545d7f7ddf13e141a2fbd47870b
utk4rshgupta <utkarshgupta2803@gmail.com>
2025-09-07 13:36:42 +0530

    New

--- Sorting/Bubblesort.java
+++ Sorting/Bubblesort.java
@@ -13,7 +13,7 @@ public class Bubblesort {
 	static void bubble (int arr[]) {
 		
 		for(int i=0;i<arr.length;i++) {
-			int tem=0;
+			int tem=0; 
 			for(int j =1;j<arr.length-i;j++) {
 				if(arr[j] < arr[j-1]) {
 					int temp= arr[j];
